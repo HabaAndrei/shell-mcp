7aef16d29b799cdff4d7dd703fba76183b08740d
HabaAndrei <habaconstantin45@gmail.com>
2026-03-24 09:12:08 +0200

    import tools in main

---
+++ tools/__init__.py
@@ -0,0 +1,11 @@
+from execut_command import execut_command
+from kill_process import kill_process
+from read_output import read_output
+from get_system import get_system
+
+__all__ = [
+    "execut_command",
+    "kill_process",
+    "read_output",
+    "get_system"
+]
